04218f3cbcbcfb3a33de294fa7b9015a7d260676
Xkower8181 <xkowerlolz@gmail.com>
2026-03-25 17:55:30 +0100

    Decompile pxtex2.c
    Co-authored-by: Mc-muffin <Mc-muffin@users.noreply.github.com>

---
+++ include/usr/local/sega/px/src/pxtex2.h
@@ -0,0 +1,15 @@
+extern unsigned int pxgPath3TransAddr; // size: 0x4, address: 0x6D1F38
+extern unsigned int pxgPath3TransInvokeCount; // size: 0x4, address: 0x6D1F3C
+
+enum PXE_ERR {
+    PXE_ERR_OK = 0,
+    PXE_ERR_INVALIDARG = -1,
+    PXE_ERR_BUSY = -2,
+    PXE_ERR_NOTREADY = -10,
+    PXE_ERR_NOTVB = -50,
+    PXE_ERR_INVALIDVBVERSION = -51,
+    PXE_ERR_TOOMANYVERTICES = -52,
+    PXE_ERR_NODATA = -53,
+    PXE_ERR_NOTENOUGHBUFFER = -54,
+    PXE_ERR_INCORRECTDBINDEX = -55,
+};
